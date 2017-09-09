bef90962cfea4442e49bf060e246b4b725d63ed2
Louis <louis@Louisde-MacBook-Air.local>
2017-09-09 20:28:41 +0800

    Initial Commit

---
+++ 猜數字/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  猜數字
+//
+//  Created by Louis on 2017/9/9.
+//  Copyright © 2017年 Louis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
